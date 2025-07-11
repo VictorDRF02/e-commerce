884c03544bddba5a011ab0792a623fbee2adb834
Víctor Rivas <VictorRivas0230@gmail.com>
2025-07-11 07:44:56 -0400

    [WIP] Add default length to shortDescription

--- src/app/shared/pipes/short-description.pipe.ts
+++ src/app/shared/pipes/short-description.pipe.ts
@@ -8,13 +8,13 @@ export class ShortDescriptionPipe implements PipeTransform {
   /**
    * Transform a string to a short description by the given length
    * @param value - The string to transform
-   * @param length - The length of the description
+   * @param length - The length of the description (default: 15)
    * @param ignoreWords - If true, the split will ignore the words, otherwise it will split by words (default: true)
    * @returns The short description
    */
   transform(
     value: string,
-    length: number,
+    length: number = 15,
     ignoreWords: boolean = true
   ): string {
     if (ignoreWords) {
