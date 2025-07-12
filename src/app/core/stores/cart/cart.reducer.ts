c9c2ce0005850db6cc21bf3fe574d5e1e32f8df8
Víctor <VictorRivas0230@gmail.com>
2025-07-12 12:40:28 -0400

    [FTR] Update store to save changes in the localStorage

--- src/app/core/stores/cart/cart.reducer.ts
+++ src/app/core/stores/cart/cart.reducer.ts
@@ -1,6 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
-import { addProduct, changeProductQuantity, removeProduct } from './cart.actions';
+import {
+  addProduct,
+  changeProductQuantity,
+  removeProduct,
+} from './cart.actions';
 import { Product } from '../../interfaces/product';
+
+/**
+ * Updates the local storage with the given state
+ * @param state - The new state
+ */
+function _updateStorage(state: CartState) {
+  localStorage.setItem('cart', JSON.stringify(state));
+}
 
 export interface CartState {
   products: Product[];
@@ -8,21 +20,19 @@ export interface CartState {
   totalItems: number;
 }
 
-export const initialState: CartState = {
-  products: [],
-  total: 0,
-  totalItems: 0,
-};
+export const initialState: CartState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart')!)
+  : { products: [], total: 0, totalItems: 0 };
 
 export const cartReducer = createReducer(
   initialState,
   on(addProduct, (state, { product }) => {
     // Check if product is already in the cart
     const existingProduct = state.products.find((p) => p.id === product.id);
-
+    let stateUpdate: CartState;
     if (existingProduct) {
       // If product is already in the cart, increment its quantity
-      return {
+      stateUpdate = {
         products: state.products.map((p) =>
           p.id === product.id ? { ...p, quantity: (p.quantity || 1) + 1 } : p
         ),
@@ -31,12 +41,14 @@ export const cartReducer = createReducer(
       };
     } else {
       // If product is not in the cart, add it
-      return {
+      stateUpdate = {
         products: [...state.products, { ...product, quantity: 1 }],
         total: state.total + product.price,
         totalItems: state.totalItems + 1,
       };
     }
+    _updateStorage(stateUpdate);
+    return stateUpdate;
   }),
 
   on(removeProduct, (state, { productId }) => {
@@ -45,13 +57,16 @@ export const cartReducer = createReducer(
     if (!productToRemove) return state;
 
     // Remove the product from the cart and update the totals
-    return {
+    const updatedState: CartState = {
       products: state.products.filter((p) => p.id !== productId),
-      total: state.total - productToRemove.price * (productToRemove.quantity || 1),
+      total:
+        state.total - productToRemove.price * (productToRemove.quantity || 1),
       totalItems: state.totalItems - (productToRemove.quantity || 1),
     };
+    _updateStorage(updatedState);
+    return updatedState;
   }),
-  
+
   on(changeProductQuantity, (state, { productId, newQuantity }) => {
     // Find the index of the product to update
     const productIndex = state.products.findIndex((p) => p.id === productId);
@@ -68,10 +83,12 @@ export const cartReducer = createReducer(
     const quantityDifference = newQuantity - oldQuantity;
 
     // Update the totals and products
-    return {
+    const updatedState: CartState = {
       products,
       total: state.total + products[productIndex].price * quantityDifference,
       totalItems: state.totalItems + quantityDifference,
     };
+    _updateStorage(updatedState);
+    return updatedState;
   })
 );
